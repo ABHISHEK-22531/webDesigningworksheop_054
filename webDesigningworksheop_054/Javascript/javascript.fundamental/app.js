f870928529bb2e48abb7e2871f4d458d3b649c48
ABHISHEK-22531 <abhisheky35514@gmail.com>
2026-04-06 01:26:18 +0530

    final changes

---
+++ webDesigningworksheop_054/Javascript/javascript.fundamental/app.js
@@ -0,0 +1,49 @@
+// var name="Abhishek";
+// var age=30;
+// var isEmployed= true;
+// console.log(name);
+// console.log(age);
+// console.log(isEmployed);
+// var age=50;
+// console.log(age);
+
+// let name="abhi";
+// let age=20;
+// console.log(name);
+// console.log(age);
+// let age=30;
+// console.log(age);
+
+// if(true){
+//     var age=30;
+// }
+//  console.log(age);
+
+// if(true){
+//     let age=30;
+// }
+//  console.log(age);
+
+// const PI=3.14;
+// console.log(PI);
+// console.log(PI);
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isemployed);
+
+
+//variable inside string
+
+// let name="abhi";
+// let age=35;
+// console.log("My name is "+name+" and my age is "+age)
+// console.log(`my name is ${name} and my name is ${age}`)
+
+//boolean
+// let data =null;
+// console.log(data);
+// console.log(typeof data);
+
+let result;
+console.log(typeof result);
---
+++ webDesigningworksheop_054/README.md
@@ -0,0 +1,1 @@
+# webDesigningworksheop_054
